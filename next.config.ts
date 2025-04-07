14f8d26b9314f42ee5fd410189d28f3eb357d396
dhorajiya8107 <142822206+dhorajiya8107@users.noreply.github.com>
2025-04-07 10:11:52 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,12 +11,9 @@ const nextConfig = {
   experimental: {
     excludeDefaultMomentLocales: true,
     optimizeCss: true,
-<<<<<<< HEAD
     outputStandalone: true,
     skipMiddlewareUrlNormalize: true,
     skipTrailingSlashRedirect: true,
-=======
->>>>>>> 3922d9e0053e87bdfb73f4334429081875dc544e
     outputFileTracingExcludes: {
       '**/_not-found/**': true
     }},
